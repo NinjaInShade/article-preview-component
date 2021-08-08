ef0fc4b4499d4d70e4d28126b5277220e180a35c
Leon Michalak <leonmichalak6@gmail.com>
2021-08-08 11:16:12 +0100

    Tooltip done, responsive too

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 function toggleTooltip() {
   document.getElementById('share-tooltip').classList.toggle('tooltip-hidden');
+  document.getElementById('share-tooltip-mobile').classList.toggle('tooltip-hidden');
 }
